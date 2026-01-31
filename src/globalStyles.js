f9c14837c7e9b2b5a2c741ca06c97ef68534f5c9
Yannick Bochatay <yannick.bochatay@free.fr>
2026-01-31 22:17:19 +0100

    add font-family to body, p and other tags

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -4,9 +4,15 @@ globalStyles.innerHTML = `
   @font-face {
     font-family: open-dyslexic;
     src: url(https://fonts.cdnfonts.com/s/29616/open-dyslexic.woff);
+  }:
+  :root {
+    --access-font-family:open-dyslexic, sans-serif;
   }
   :root.dyslexic {
-    font-family:open-dyslexic, sans-serif;
+    font-family:var(--access-font-family);
+    body, main, article, section, aside, p {
+      font-family:var(--access-font-family);
+    }
   }
   :root.invertedColors {
     filter:invert(1);
