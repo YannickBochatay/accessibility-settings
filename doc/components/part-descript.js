182223fa0d72de0644e3ed08eaf21c9ef86f184b
Yannick Bochatay <yannick.bochatay@free.fr>
2026-03-12 07:14:43 +0100

    End of translation

--- doc/components/part-descript.js
+++ doc/components/part-descript.js
@@ -1,15 +1,25 @@
 const template = document.createElement("template");
 
 template.innerHTML = `
-  <strong class="field-name"></strong> : élément <code>&lt;div></code> du champ <span class="field-descript"></span>
+  <strong class="field-name"></strong> :
+  <span lang="fr">element <code>&lt;div></code> du champ</span>
+  <span lang="en"><code>&lt;div></code> element of field</span>
+  <span lang="fr" class="field-descript"></span>
+  <span lang="en" class="field-descript"></span>
   <ul>
     <li>
       <strong><span class="field-name"></span>-input</strong> :
-      élément <code>&lt;input></code> du champ <span class="field-descript"></span>
+      <span lang="fr">element <code>&lt;input></code> du champ</span>
+      <span lang="en"><code>&lt;input></code> element of field</span>
+      <span lang="fr" class="field-descript"></span>
+      <span lang="en" class="field-descript"></span>
     </li>
     <li>
       <strong><span class="field-name"></span>-label</strong> :
-      élément <code>&lt;label></code> du champ <span class="field-descript"></span>
+      <span lang="fr">element <code>&lt;label></code> du champ</span>
+      <span lang="en"><code>&lt;label></code> element of field</span>
+      <span lang="fr" class="field-descript"></span>
+      <span lang="en" class="field-descript"></span>
     </li>
   </ul>
 `
@@ -25,8 +35,11 @@ class PartDescript extends HTMLElement {
     for (const node of this.querySelectorAll(".field-name")) {
       node.textContent = this.getAttribute("field-name");
     }
-    for (const node of this.querySelectorAll(".field-descript")) {
-      node.textContent = this.getAttribute("field-descript");
+    for (const node of this.querySelectorAll("[lang=fr].field-descript")) {
+      node.textContent = this.getAttribute("field-descript-fr");
+    }
+    for (const node of this.querySelectorAll("[lang=en].field-descript")) {
+      node.textContent = this.getAttribute("field-descript-en");
     }
   }
 }
