1477523298980aa62d92ffd17fb3575383f195be
Yannick Bochatay <yannick.bochatay@free.fr>
2026-03-15 11:28:19 +0100

    add copy button

--- doc/components/code-example.js
+++ doc/components/code-example.js
@@ -2,11 +2,14 @@ import hljs from "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/es
 
 const template = document.createElement("template");
 
+const label = "Copy code to clipboard";
+
 template.innerHTML = `
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/a11y-light.min.css">
   <style>
     :host {
       display:block;
+      position:relative;
 
       & > div {
         overflow-x:auto;
@@ -20,11 +23,35 @@ template.innerHTML = `
             width:fit-content;
           }
         }
+
+        button {
+          position:absolute;
+          top:0;
+          right:0;
+          border:none;
+
+          svg {
+            stroke-width:2;
+            stroke:#555;
+            fill:none;
+            stroke-linecap:round;
+            stroke-linejoin:round;
+          }
+        }
       }
+    }
+    :host([nocopy]) > div > button {
+      display:none;
     }
   </style>
   <div tabindex="0">
     <pre><code class="language-xxx"></code></pre>
+    <button title="Copy" aria-label="${label}">
+      <svg width="24" height="24">
+        <path d="M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"></path>
+        <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
+      </svg>
+    </button>
   </div>
   <slot></slot>
 `
@@ -41,6 +68,28 @@ class CodeExample extends HTMLElement {
     return this.getAttribute("language");
   }
 
+  copyCode = async () => {
+    const { textContent } = this.shadowRoot.querySelector("code");
+    const btn = this.shadowRoot.querySelector("button");
+    
+    try {
+      await navigator.clipboard.writeText(textContent);
+
+      const icon = btn.firstElementChild;
+      
+      btn.textContent = 'Copied !';
+      btn.setAttribute('aria-label', 'Code copied');
+      
+      setTimeout(() => {
+          btn.replaceChildren(icon);
+          btn.setAttribute('aria-label', label);
+      }, 2000);
+
+    } catch (e) {
+      alert('Unable to copy code. Please copy it manually.');
+    }
+  }
+
   connectedCallback() {
     const code = this.shadowRoot.querySelector("code");
     const slot = this.shadowRoot.querySelector("slot");    
@@ -48,6 +97,9 @@ class CodeExample extends HTMLElement {
     code.append(...slot.assignedNodes());
     slot.remove();
     hljs.highlightElement(code);
+
+    const button = this.shadowRoot.querySelector("button");
+    button.addEventListener("click", this.copyCode);
   }
 }
 
